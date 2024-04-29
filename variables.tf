a06fd5d009d1b0681c924302ac25a878eb31f437
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2024-04-29 23:00:43 +1000

    network-infra-deployment

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "name" {
     type = string
     description = "Name to tag resources with"
-    default = "three-tier-aus"
+    default = "three-tier"
 }
 
 variable "tfe-org-name" {
